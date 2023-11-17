7635e82642ad5050e6a03a018338e25b9ddb1950
Wesley Teles <wesley.teles.dev@gmail.com>
2023-11-17 19:57:57 -0300

    image path correction

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,26 +1,31 @@
+import client1Image from "../assets/Img/client-1.svg";
+import client2Image from "../assets/Img/client-2.svg";
+import client3Image from "../assets/Img/client-3.svg";
+import client4Image from "../assets/Img/client-4.svg";
+
 export const projectsData = [
   {
     id: 1,
-    title: "WebSite FR do Sol Nascente ",
+    title: "WebSite FR do Sol Nascente",
     description: "Created using Html5, Css3, JavaScript, PhpMailer and Mysql",
-    image: "../assets/Img/client-1.svg",
+    image: client1Image,
   },
   {
     id: 2,
     title: "Ecoomerce Wordpress",
     description: "Created using Wordpress, Woocommerce and ElementorPro",
-    image: "../assets/Img/client-4.svg",
+    image: client4Image,
   },
   {
     id: 3,
     title: "FRSN - Chek-in App",
     description: "Created using ReactNative, FirebaseAuth, NodeJs and Mysql",
-    image: "../assets/Img/client-3.svg",
+    image: client3Image,
   },
   {
     id: 4,
     title: "Lading Page Tatiane Muller",
     description: "Created using Html5, Css3, JavaScript",
-    image: "../assets/Img/client-2.svg",
+    image: client2Image,
   },
 ];
