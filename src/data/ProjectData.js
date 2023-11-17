8082fcf35cd5839c99c2827caad33be1bae8dfb6
Wesley Teles <wesley.teles.dev@gmail.com>
2023-11-17 19:46:59 -0300

    Testing UrlImage

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -3,24 +3,24 @@ export const projectsData = [
     id: 1,
     title: "WebSite FR do Sol Nascente ",
     description: "Created using Html5, Css3, JavaScript, PhpMailer and Mysql",
-    image: "src/assets/Img/client-1.svg",
+    image: "./src/assets/Img/client-1.svg",
   },
   {
     id: 2,
     title: "Ecoomerce Wordpress",
     description: "Created using Wordpress, Woocommerce and ElementorPro",
-    image: "src/assets/Img/client-4.svg",
+    image: "./src/assets/Img/client-4.svg",
   },
   {
     id: 3,
     title: "FRSN - Chek-in App",
     description: "Created using ReactNative, FirebaseAuth, NodeJs and Mysql",
-    image: "src/assets/Img/client-3.svg",
+    image: "./src/assets/Img/client-3.svg",
   },
   {
     id: 4,
     title: "Lading Page Tatiane Muller",
     description: "Created using Html5, Css3, JavaScript",
-    image: "src/assets/Img/client-2.svg",
+    image: "./src/assets/Img/client-2.svg",
   },
 ];
